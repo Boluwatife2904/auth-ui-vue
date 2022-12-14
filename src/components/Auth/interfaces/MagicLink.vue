d0eea3891ecc68eb6e267d5ff96b5a05959d5ffa
Boluwatife2904 <sanvicola2000@gmail.com>
2022-12-14 10:06:46 +0100

    feat: implement the logic for sending magic link to user emails

--- src/components/Auth/interfaces/MagicLink.vue
+++ src/components/Auth/interfaces/MagicLink.vue
@@ -9,19 +9,38 @@ defineProps<{
     i18n: I18nVariables;
 }>();
 
-const { showLinks } = inject("props");
+const { showLinks, supabaseClient } = inject("props");
 const { changeView } = inject("view");
+const { setIsLoading } = inject("loading");
+
+const sendMagicLink = async () => {
+    setIsLoading(true);
+    error.value = "";
+    message.value = "";
+    const { error: magicLinkError } = await supabaseClient.auth.signInWithOtp({
+        email: email.value,
+        options: {
+            emailRedirectTo: "https://google.com/magic-link",
+        },
+    });
+    if (magicLinkError) {
+        error.value = magicLinkError.message;
+    } else {
+        message.value = "Check your email for the magic link";
+    }
+    setIsLoading(false);
+};
 </script>
 
 <template>
-    <form id="auth-magic-link">
+    <form id="auth-magic-link" @submit.prevent="sendMagicLink">
         <Container gap="large" direction="vertical">
             <Container gap="large" direction="vertical">
                 <div>
                     <Label label-for="email" :label="i18n['magic_link']?.email_input_label" />
                     <Input v-model="email" name="email" id="email" type="email" :placeholder="i18n['magic_link']?.email_input_label" />
                 </div>
-                <Button type="button" :loading="false" variant="primary">{{ i18n["magic_link"]?.button_label }}</Button>
+                <Button type="submit" :loading="false" variant="primary">{{ i18n["magic_link"]?.button_label }}</Button>
             </Container>
             <template v-if="showLinks">
                 <Anchor href="#auth-sign-up" @click.prevent="changeView('sign_up')"> {{ i18n["sign_up"]?.link_text }} </Anchor>
